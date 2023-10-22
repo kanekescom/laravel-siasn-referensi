0173695e086820758f8572aade61ff74d584507e
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2023-10-22 13:30:57 +0700

    Rewrite to use spatie laravel package tools

---
+++ config/siasn-referensi.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models
+    |--------------------------------------------------------------------------
+    |
+    | This option controls which model will be used when using this library.
+    |
+    */
+
+    'agama' => Kanekescom\Siasn\Referensi\Models\Agama::class,
+    'alasanHukumanDisiplin' => Kanekescom\Siasn\Referensi\Models\AlasanHukumanDisiplin::class,
+    'asnJenisJabatan' => Kanekescom\Siasn\Referensi\Models\AsnJenisJabatan::class,
+    'asnJenjangJabatan' => Kanekescom\Siasn\Referensi\Models\AsnJenjangJabatan::class,
+    'eselon' => Kanekescom\Siasn\Referensi\Models\Eselon::class,
+    'golongan' => Kanekescom\Siasn\Referensi\Models\Golongan::class,
+    'instansi' => Kanekescom\Siasn\Referensi\Models\Instansi::class,
+    'jabatanFungsional' => Kanekescom\Siasn\Referensi\Models\JabatanFungsional::class,
+    'jabatanFungsionalUmum' => Kanekescom\Siasn\Referensi\Models\JabatanFungsionalUmum::class,
+    'jenisAnak' => Kanekescom\Siasn\Referensi\Models\JenisAnak::class,
+    'jenisDiklat' => Kanekescom\Siasn\Referensi\Models\JenisDiklat::class,
+    'jenisHukuman' => Kanekescom\Siasn\Referensi\Models\JenisHukuman::class,
+    'jenisJabatan' => Kanekescom\Siasn\Referensi\Models\JenisJabatan::class,
+    'kanreg' => Kanekescom\Siasn\Referensi\Models\Kanreg::class,
+    'kedudukanHukum' => Kanekescom\Siasn\Referensi\Models\KedudukanHukum::class,
+    'kelJabatan' => Kanekescom\Siasn\Referensi\Models\KelJabatan::class,
+    'latihanStruktural' => Kanekescom\Siasn\Referensi\Models\LatihanStruktural::class,
+    'lokasi' => Kanekescom\Siasn\Referensi\Models\Lokasi::class,
+    'pendidikan' => Kanekescom\Siasn\Referensi\Models\Pendidikan::class,
+    'refDokumen' => Kanekescom\Siasn\Referensi\Models\RefDokumen::class,
+    'refJenjangJf' => Kanekescom\Siasn\Referensi\Models\RefJenjangJf::class,
+    'satuanKerja' => Kanekescom\Siasn\Referensi\Models\SatuanKerja::class,
+    'tingkatPendidikan' => Kanekescom\Siasn\Referensi\Models\TingkatPendidikan::class,
+
+];
