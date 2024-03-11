f7c64f1deef4c5fcfb58890cd25e1d212d5abfd0
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2024-03-11 08:13:19 +0700

    feature: add config for panel

--- src/SiasnReferensiPanelProvider.php
+++ src/SiasnReferensiPanelProvider.php
@@ -26,34 +26,43 @@ class SiasnReferensiPanelProvider extends PanelProvider
 
         return $panel
             ->default()
-            ->id(config('siasn-referensi.filament.id'))
+            ->id('siasn-referensi')
             ->path(config('siasn-referensi.filament.path'))
+            ->domain(config('siasn-referensi.filament.domain'))
             ->profile(isSimple: false)
             ->login()
+            ->brandName(config('siasn-referensi.filament.brandName'))
             ->brandLogo(config('siasn-referensi.filament.brandLogo'))
+            ->brandLogoHeight(config('siasn-referensi.filament.brandLogoHeight'))
             ->favicon(config('siasn-referensi.filament.favicon'))
             ->colors(config('siasn-referensi.filament.colors'))
-            ->topbar(config('siasn-referensi.filament.topbar'))
-            ->sidebarCollapsibleOnDesktop()
+            ->darkMode(config('siasn-referensi.filament.darkMode.enabled'))
+            ->topbar(config('siasn-referensi.filament.topbar.enabled'))
+            ->topNavigation(config('siasn-referensi.filament.topNavigation.enabled'))
+            ->breadcrumbs(config('siasn-referensi.filament.breadcrumbs.enabled'))
+            ->databaseNotifications(config('siasn-referensi.filament.databaseNotifications.enabled'))
+            ->databaseNotificationsPolling(config('siasn-referensi.filament.databaseNotifications.polling'))
+            ->spa(config('siasn-referensi.filament.spa.enabled'))
+            ->unsavedChangesAlerts(config('siasn-referensi.filament.unsavedChangesAlerts.enabled'))
+            ->databaseTransactions(config('siasn-referensi.filament.databaseTransactions.enabled'))
+            ->sidebarCollapsibleOnDesktop(config('siasn-referensi.filament.sidebarCollapsibleOnDesktop.enabled'))
+            ->sidebarFullyCollapsibleOnDesktop(config('siasn-referensi.filament.sidebarFullyCollapsibleOnDesktop.enabled'))
+            ->navigation(config('siasn-referensi.filament.navigation.enabled'))
+            ->collapsibleNavigationGroups(config('siasn-referensi.filament.collapsibleNavigationGroups.enabled'))
+            ->navigationGroups(config('siasn-referensi.filament.navigationGroups'))
             ->discoverResources(in: __DIR__.'/Filament/Resources', for: 'Kanekescom\\Siasn\\Referensi\\Filament\\Resources')
             ->discoverPages(in: __DIR__.'/Filament/Pages', for: 'Kanekescom\\Siasn\\Referensi\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: __DIR__.'/Filament/Widgets', for: 'Kanekescom\\Siasn\\Referensi\\Filament\\Widgets')
-            ->topbar(config('siasn-referensi.filament.topbar'))
-            ->sidebarCollapsibleOnDesktop()
             ->widgets([
                 //
             ])
-            ->navigationGroups(config('siasn-referensi.filament.navigationGroups'))
             ->plugins([
                 \ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin::make()
                     ->usingPage(\Kanekescom\Siasn\Referensi\Filament\Pages\Backups::class),
             ])
-            ->spa()
-            ->unsavedChangesAlerts()
-            ->databaseTransactions()
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
