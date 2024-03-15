f5b57e3785937b89c898206a9bdb4e4ea8139e31
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2024-03-15 13:25:02 +0700

    feature: add job

---
+++ src/Jobs/PullEselonJob.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kanekescom\Siasn\Referensi\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Kanekescom\Siasn\Referensi\Services\PullEselonService;
+
+class PullEselonJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        $this->onQueue('processing');
+    }
+
+    public function handle(): void
+    {
+        PullEselonService::execute();
+    }
+}
---
+++ src/Jobs/PullRefJenjangJfJob.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kanekescom\Siasn\Referensi\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Kanekescom\Siasn\Referensi\Services\PullRefJenjangJfService;
+
+class PullRefJenjangJfJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        $this->onQueue('processing');
+    }
+
+    public function handle(): void
+    {
+        PullRefJenjangJfService::execute();
+    }
+}
